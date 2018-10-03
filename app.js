3dd5f55253d3e4126184c0a5c8063b05b10d4358
Simon Ekizean <simon.ekizean@clarmeont.se>
2018-10-03 21:59:24 +0200

    Fixed slide effect

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-/* Scrolla smooth när klickar på en anchor link */
+$( document ).ready(function() {
 
+/* Scrolla smooth när klickar på en anchor link */
 $(document).on('click', 'a.smooth-scroll',function (event) {
     event.preventDefault();
 
@@ -11,4 +12,24 @@ $(document).on('click', 'a.smooth-scroll',function (event) {
 /* Fixar så att när man klickar på ett meny-item i mobil-version så stängs menylistan */
 $('#top-nav > li > .dropdown-toggle').click(function () {
    window.location = $(this).attr('href');
+});
+var imvisible = false
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 780 && imvisible == false)  {
+    $('#davidsdiv').effect("slide", {
+        direction: "right",
+        mode: 'show'
+    });
+      imvisible = true
+  } 
+    else if (y <= 780 && imvisible == true) {
+        imvisible = false
+        $('#davidsdiv').effect("slide", {
+        direction: "right",
+        mode: 'hide'
+    });
+    }
+});
+
 });
