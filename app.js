cfdf42e7bd67c8bd1c432f99d6428e842d835dbc
Simon Ekizean <simon.ekizean@clarmeont.se>
2018-10-04 11:12:16 +0200

    Loads of changes

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 $( document ).ready(function() {
 
-/* Scrolla smooth när klickar på en anchor link */
-$(document).on('click', 'a.smooth-scroll',function (event) {
-    event.preventDefault();
+    /* Scrolla smooth när klickar på en anchor link */
+    $(document).on('click', 'a.smooth-scroll',function (event) {
+        event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
-
-/* Fixar så att när man klickar på ett meny-item i mobil-version så stängs menylistan */
-$('#top-nav > li > .dropdown-toggle').click(function () {
-   window.location = $(this).attr('href');
-});
-var imvisible = false
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 780 && imvisible == false)  {
-    $('#davidsdiv').effect("slide", {
-        direction: "right",
-        mode: 'show'
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
     });
-      imvisible = true
-  } 
-    else if (y <= 780 && imvisible == true) {
-        imvisible = false
+
+    /* Fixar så att när man klickar på ett meny-item i mobil-version så stängs menylistan */
+    $('#top-nav > li > .dropdown-toggle').click(function () {
+       window.location = $(this).attr('href');
+    });
+    
+    var imvisible = false;
+    $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      if (y > 650 && imvisible == false)  {
         $('#davidsdiv').effect("slide", {
-        direction: "right",
-        mode: 'hide'
+            direction: "right",
+            mode: 'show'
+        });
+          imvisible = true
+      } 
+        else if (y <= 650 && imvisible == true) {
+            imvisible = false;
+            $('#davidsdiv').effect("slide", {
+            direction: "right",
+            mode: 'hide'
+        });
+        }
     });
-    }
-});
-
+    
+        var invisible = false;
+    $(document).scroll(function() {
+      var y = $(this).scrollTop();
+      if (y > 650 && invisible == false)  {
+        $('#bokabordmobil').effect("slide", {
+            direction: "up",
+            mode: 'show'
+        });
+          invisible = true
+      } 
+        else if (y <= 650 && invisible == true) {
+            invisible = false;
+            $('#bokabordmobil').effect("slide", {
+            direction: "up",
+            mode: 'hide'
+            });
+        }
+    });
+    
 });
