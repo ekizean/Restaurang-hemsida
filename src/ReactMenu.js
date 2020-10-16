659a12670c1f1ba43ec0466a9e1e7073f87d2955
Simon Ekizean <ekizean@gmail.com>
2020-10-16 08:38:30 +0200

    Just some formatting

--- src/ReactMenu.js
+++ src/ReactMenu.js
@@ -1,92 +1,92 @@
-"use strict";
-import CourseMenu from "./CourseMenu.js";
-import NavButton from "./NavButton.js";
+'use strict';
+import CourseMenu from './CourseMenu.js';
+import NavButton from './NavButton.js';
 
 class ReactMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: "Antipasti",
-      menuData: [],
-      dishes: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: 'Antipasti',
+            menuData: [],
+            dishes: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getMenuData();
+    }
+
+    getMenuData() {
+        const url = '../Meny hemsida.xlsx';
+
+        /* set up async GET request */
+        const req = new XMLHttpRequest();
+        req.open('GET', url, true);
+        req.responseType = 'arraybuffer';
+
+        req.onload = (e) => {
+            const data = new Uint8Array(req.response);
+            const workbook = XLSX.read(data, { type: 'array' });
+
+            /* DO SOMETHING WITH workbook HERE */
+            const first_sheet = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[first_sheet];
+            const menuJson = XLSX.utils.sheet_to_json(worksheet);
+
+            this.setState({ menuData: menuJson }, () => {
+                this.setCurrentDishes();
+            });
+        };
+        req.send();
+    }
+
+    setCurrentDishes = () => {
+        const dishes = this.state.menuData.filter((dish) => {
+            return dish.course.toLowerCase() == this.state.course.toLowerCase();
+        });
+
+        this.setState({ dishes });
     };
-  }
 
-  componentDidMount() {
-    this.getMenuData();
-  }
-
-  getMenuData() {
-    const url = "../Meny hemsida.xlsx";
-
-    /* set up async GET request */
-    const req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.responseType = "arraybuffer";
-
-    req.onload = (e) => {
-      const data = new Uint8Array(req.response);
-      const workbook = XLSX.read(data, { type: "array" });
-
-      /* DO SOMETHING WITH workbook HERE */
-      const first_sheet = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[first_sheet];
-      const menuJson = XLSX.utils.sheet_to_json(worksheet);
-
-      this.setState({ menuData: menuJson }, () => {
-        this.setCurrentDishes();
-      });
+    clickOnNavButton = (course) => {
+        this.setState({ course }, () => {
+            this.setCurrentDishes();
+        });
     };
-    req.send();
-  }
 
-  setCurrentDishes = () => {
-    const dishes = this.state.menuData.filter((dish) => {
-      return dish.course.toLowerCase() == this.state.course.toLowerCase();
-    });
+    getUniqueCourses = () => {
+        const uniqueCourses = new Set();
+        this.state.menuData.forEach((dish) => {
+            uniqueCourses.add(dish.course);
+        });
 
-    this.setState({ dishes });
-  };
+        return Array.from(uniqueCourses);
+    };
 
-  clickOnNavButton = (course) => {
-    this.setState({ course }, () => {
-      this.setCurrentDishes();
-    });
-  };
+    render() {
+        const navButtons = this.getUniqueCourses();
 
-  getUniqueCourses = () => {
-    const uniqueCourses = new Set();
-    this.state.menuData.forEach((dish) => {
-      uniqueCourses.add(dish.course);
-    });
-
-    return Array.from(uniqueCourses);
-  };
-
-  render() {
-    const navButtons = this.getUniqueCourses();
-
-    return (
-      <div className="menu">
-        <div className="course-navigator">
-          <div className="course-row">
-            {navButtons.map((navButton) => {
-              return (
-                <NavButton
-                  thisCourse={navButton}
-                  clickOnNavButton={this.clickOnNavButton}
-                  selectedCourse={this.state.course}
-                  key={`${navButton}`}
-                />
-              );
-            })}
-          </div>
-        </div>
-        <CourseMenu dishes={this.state.dishes} />
-      </div>
-    );
-  }
+        return (
+            <div className="menu">
+                <div className="course-navigator">
+                    <div className="course-row">
+                        {navButtons.map((navButton) => {
+                            return (
+                                <NavButton
+                                    thisCourse={navButton}
+                                    clickOnNavButton={this.clickOnNavButton}
+                                    selectedCourse={this.state.course}
+                                    key={`${navButton}`}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+                <CourseMenu dishes={this.state.dishes} />
+            </div>
+        );
+    }
 }
 
-let domContainer = document.querySelector("#react-menu");
+let domContainer = document.querySelector('#react-menu');
 ReactDOM.render(<ReactMenu />, domContainer);
